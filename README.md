ffc82d54c01e81de997781b6523c0c4afd021d0d
PJ O'Leary <peej@oleary.com>
2023-11-15 17:23:05 -0800

    App Framework Set Up fix

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Product-Detail-Page
-Front end capstone project that replicates an online retail store's product details page 
+Front end capstone project that replicates an online retail store's product details page
 
 # Overview
 Our outdated client-facing retail web-portal has become significantly outdated and has been proven to be hurting sales numbers. Project Atelier comprises a complete redesign of the retail portal designed to address this concern and modernize the site.
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable */
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  devServer: {
+    port: 3000
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
+    },
+    {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"]
+    }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
