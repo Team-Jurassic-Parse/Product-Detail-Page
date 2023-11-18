b59fb950a0eaccddb6298e0cc983acbfebf2a69c
PJ O'Leary <peej@oleary.com>
2023-11-17 16:45:22 -0800

    Ticket zrVvbjjI -- Added Initial Fetches for Overview

--- src/components/Overview/index.test.jsx
+++ src/components/Overview/index.test.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { describe, expect, it } from '@jest/globals';
+import {
+  describe,
+  expect, it,
+  beforeEach,
+} from '@jest/globals';
 import { render, screen } from '@testing-library/react';
 import Overview from './index.jsx'; // eslint-disable-line
 
 describe('Overview', () => {
-  it('exists', () => {
+  beforeEach(() => {
     render(<Overview />);
+  });
+  it('exists', () => {
     expect(screen.getAllByText('Overview').toBeTruthy);
+  });
+  it('fetches product info on productId change', () => {
+    expect(Promise.resolve(setTimeout(() => Overview.productInfo, 1000))).toBeTruthy();
+  });
+  it('fetches product styles on productId change', () => {
+    expect(Promise.resolve(setTimeout(() => Overview.productInfo, 1000))).toBeTruthy();
   });
 });
