b4d9611c3f5906a7b761b0fbc79689520593a49a
Kyle Mak <kjmmak@gmail.com>
2023-11-18 21:36:52 -0800

    fetched questions and added a QuestionsList component

---
+++ src/components/QuestionsAndAnswers/QuestionsList.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+function QuestionsList() {
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
+
+export default QuestionsList;
--- src/components/QuestionsAndAnswers/index.jsx
+++ src/components/QuestionsAndAnswers/index.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function QuestionsAndAnswers() {
+import useServerFetch from '../../hooks/useServerFetch.js'; //eslint-disable-line
+import QuestionsList from './QuestionsList.jsx'; //eslint-disable-line
+
+function QuestionsAndAnswers({ productId }) {//eslint-disable-line
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState([]);
+  const answerFetchController = new AbortController();
+  const [questionId, setQuestionId] = useState('');
+  const questionFetchController = new AbortController();
+
+  console.log('this is the productId:', productId);
+  useEffect(() => {
+    if (productId) {
+      useServerFetch('get', `qa/questions?product_id=${productId}`, {}, questionFetchController)
+        .then((response) => {
+          console.log('this is the response for question get:', response.data);
+          console.log('this is the id to the above question:', response.data.results[0].question_id);
+          setQuestions(response.data);
+          setQuestionId(response.data.results[0]);
+          console.log('this is the question after setquesitons:', questions);
+        })
+        .catch((err) => {
+          console.log('this is the err for getting questions:', err);
+          setQuestions(null);
+        });
+    }
+    return (() => {
+      questionFetchController.abort();
+    });
+  }, [productId]);
   return (
     <>
-      <p>QuestionsAndAnswers. Testing Change</p>
-      <p>QuestionsAndAnswers</p>
+      <h4>QUESTIONS & ANSWERS </h4>
+      <QuestionsList questions={questions} />
     </>
   );
 }
