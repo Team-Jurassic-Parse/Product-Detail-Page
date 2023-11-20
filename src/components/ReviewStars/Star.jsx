325ede5581a7e6f820339abe002e86770b29627a
Xianyi Liang <liangxianyi2011@gmail.com>
2023-11-20 11:26:20 -0800

    add aria label to Star component for testing scalibility, and for screen reader accessibility

--- src/components/ReviewStars/Star.jsx
+++ src/components/ReviewStars/Star.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 function Star({fill}) { // eslint-disable-line
   if (fill > 0.9) {
-    return (<div>🌕</div>);
+    return (<div aria-label="Full star">🌕</div>);
   }
   if (fill > 0.6) {
-    return (<div>🌖</div>);
+    return (<div aria-label="Three quarters star">🌖</div>);
   }
   if (fill > 0.4) {
-    return (<div>🌗</div>);
+    return (<div aria-label="Half star">🌗</div>);
   }
   if (fill > 0.1) {
-    return (<div>🌘</div>);
+    return (<div aria-label="Quarter star">🌘</div>);
   }
-  return (<div>🌑</div>);
+  return (<div aria-label="Empty star">🌑</div>);
 }
 
 export default Star;
