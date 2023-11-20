f634287935603a92573a897625f18ed7da0d1114
Xianyi Liang <liangxianyi2011@gmail.com>
2023-11-20 12:06:33 -0800

    Add a widget switch button for development

--- src/components/RatingAndReviews/index.jsx
+++ src/components/RatingAndReviews/index.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import RatingSummary from './RatingSummary.jsx'; // eslint-disable-line
 import ReviewsList from './ReviewsList.jsx'; // eslint-disable-line
 import ReviewForm from './ReviewForm.jsx'; // eslint-disable-line
-import Modal from '../UI/Modal.jsx' // eslint-disable-line
+import Modal from '../UI/Modal.jsx'; // eslint-disable-line
 
 const Wrapper = styled.div`
   border: 1px solid;
@@ -20,23 +20,37 @@ const AddFormBtn = styled.button`
   margin: auto;
 `;
 
-function RatingAndReviews({ productId, productName='Anonymous' }) { // eslint-disable-line
+function RatingAndReviews({ productId, productName = 'Anonymous' }) { // eslint-disable-line
 
   const [showForm, setShowForm] = useState(false);
-  const openModal = () => { setShowForm(true); };
-  const closeModal = () => { setShowForm(false); };
+  const openModal = () => {
+    setShowForm(true);
+  };
+  const closeModal = () => {
+    setShowForm(false);
+  };
+  const [showWidget, setShowWidget] = useState(false);
 
   return (
-    <Wrapper>
-      <SummaryAndListWrapper>
-        <RatingSummary productId={productId} />
-        <ReviewsList productId={productId} />
-      </SummaryAndListWrapper>
-      <BtnWrapper>
-        <AddFormBtn onClick={openModal}>Add +</AddFormBtn>
-      </BtnWrapper>
-      {showForm && <Modal handleClose={closeModal}><ReviewForm productName={productName} /></Modal>}
-    </Wrapper>
+    <>
+      <button type="button" onClick={() => { setShowWidget((cur) => !cur); }}>Toggle Lance&apos;s widget</button>
+      {showWidget && (
+        <Wrapper>
+          <SummaryAndListWrapper>
+            <RatingSummary productId={productId} />
+            <ReviewsList productId={productId} />
+          </SummaryAndListWrapper>
+          <BtnWrapper>
+            <AddFormBtn onClick={openModal}>Add +</AddFormBtn>
+          </BtnWrapper>
+          {showForm && (
+            <Modal handleClose={closeModal}>
+              <ReviewForm productName={productName} />
+            </Modal>
+          )}
+        </Wrapper>
+      )}
+    </>
   );
 }
 
