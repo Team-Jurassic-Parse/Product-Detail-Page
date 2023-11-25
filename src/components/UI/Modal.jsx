fa201e73305134587f3d02b0cf51e2a9299aeccc
bzw24 <brucewong24@gmail.com>
2023-11-24 19:20:43 -0800

    implemented comparison modal, caching, 404 bug fix

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,42 +1,46 @@
 import React from 'react';
 
-function Modal({children, handleClose}) { // eslint-disable-line
+function Modal({ children, handleClose }) {
+  // eslint-disable-line
 
   return (
-    (
-      <div>
-        <button
-          type="button"
-          onClick={() => {
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          handleClose();
+        }}
+        onKeyUp={(e) => {
+          if (e.key === 'Escape') {
             handleClose();
-          }}
-          onKeyUp={(e) => {
-            if (e.key === 'Escape') {
-              handleClose();
-            } else if (e.key === ' ' || e.key === 'Spacebar') {
-              e.preventDefault();
-            }
-          }}
-          style={
-          {
-            position: 'fixed',
-            top: '0',
-            left: '0',
-            background: 'rgba(0, 0, 0, 0.5)',
-            borderStyle: 'none',
-            inset: 0,
-            width: '100vw',
-            height: '100vh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
+          } else if (e.key === ' ' || e.key === 'Spacebar') {
+            e.preventDefault();
           }
-        }
+        }}
+        style={{
+          position: 'fixed',
+          top: '0',
+          left: '0',
+          background: 'rgba(0, 0, 0, 0.5)',
+          borderStyle: 'none',
+          inset: 0,
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
         >
-          <div onClick={(e) => {e.stopPropagation()}}>{children}</div> {/* eslint-disable-line */}
-        </button>
-      </div>
-    )
+          {children}
+        </div>{' '}
+        {/* eslint-disable-line */}
+      </button>
+    </div>
   );
 }
 
