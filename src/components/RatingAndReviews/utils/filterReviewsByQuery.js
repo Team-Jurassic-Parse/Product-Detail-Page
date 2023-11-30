d5296e50b8bea859cc845d4ac7d969ed95aaafc3
Xianyi Liang <liangxianyi2011@gmail.com>
2023-11-30 15:12:57 -0800

    make it case insensitive.

--- src/components/RatingAndReviews/utils/filterReviewsByQuery.js
+++ src/components/RatingAndReviews/utils/filterReviewsByQuery.js
@@ -1,6 +1,7 @@
 export default function filterReviewsByQuery(reviews, query) {
   if (!query) return reviews;
   return reviews?.filter(
-    (review) => (review?.summary.includes(query) || review?.body.includes(query)),
+    (review) => (review?.summary.toLowerCase().includes(query.toLowerCase())
+      || review?.body.toLowerCase().includes(query.toLowerCase())),
   );
 }
